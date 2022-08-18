fdd8b936fb9c1433d35a2b093d500878782b4b70
Vish katyan <82721870+vishkatyan@users.noreply.github.com>
2022-08-18 17:59:49 +0530

    Create functions_basic.java

---
+++ functions_basic.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class functions {
+    public static int add(int a, int b){
+        int sum = a+b;
+        return sum;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc. nextInt();
+        int b = sc. nextInt();
+        add(a,b);
+    }
+}
